ba052b812ef630e66c94502c4308ab38dbb12e54
aakanksha-gr <j.aakanksha007@gmail.com>
2018-04-27 14:40:48 +0530

    link set 27-04 gaurav

--- app/src/main/java/com/pcdgroup/hp/pcd_group/Product/ViewImage.java
+++ app/src/main/java/com/pcdgroup/hp/pcd_group/Product/ViewImage.java
@@ -65,7 +65,7 @@ public class ViewImage extends AppCompatActivity implements CustomListAdapter.Da
     String IdHolder;
 
     // Http URL for delete Already Open Client Record.
-    String HttpUrlDeleteRecord = "http://pcddata-001-site1.1tempurl.com/DeleteClient.php";
+    String HttpUrlDeleteRecord = "http://dert.co.in/gFiles/DeleteClient.php";
 
     ProgressDialog progressDialog2;
     RecyclerView recyclerView;
@@ -113,7 +113,7 @@ public class ViewImage extends AppCompatActivity implements CustomListAdapter.Da
 
 //        actionModeCallback = new ActionModeCallback();
 
-        adapter = new CustomListAdapter(this,this, localEntity,this);
+        adapter = new CustomListAdapter(this, localEntity,this);
         listView.setAdapter(adapter);
 
         //Allow network in main thread
@@ -285,10 +285,6 @@ public class ViewImage extends AppCompatActivity implements CustomListAdapter.Da
         }
     }
 
-    @Override
-    public void onDataSelected(DataAdapter dataAdapter) {
-
-    }
 
     @Override
     public void onDataSelected(Entity dataAdapter) {
@@ -296,149 +292,5 @@ public class ViewImage extends AppCompatActivity implements CustomListAdapter.Da
     }
 
 
-    @Override
-    public void onIconClicked(int position) {
-        /*if (actionMode == null) {
-            actionMode = startSupportActionMode(actionModeCallback);
-        }
 
-        toggleSelection(position);*/
-    }
-
-    @Override
-    public void onRowLongClicked(int position) {
-        // long press is performed, enable action mode
-//        enableActionMode(position);
-    }
-
-    private void enableActionMode(int position) {
-        /*if (actionMode == null) {
-            actionMode = startSupportActionMode(actionModeCallback);
-        }
-        toggleSelection(position);*/
-    }
-
-    private void toggleSelection(int position) {
-       /* adapter.toggleSelection(position);
-        int count = adapter.getSelectedItemCount();
-
-        if (count == 0) {
-            actionMode.finish();
-        } else {
-            actionMode.setTitle(String.valueOf(count));
-            actionMode.invalidate();
-        }*/
-    }
-
-    @Override
-    public void onIconImportantClicked(int position) {
-       /* // Star icon is clicked,
-        // mark the message as important
-        Entity message = localEntity.get(position);
-        localEntity.set(position, message);
-        adapter.notifyDataSetChanged();*/
-    }
-
-    @Override
-    public void onMessageRowClicked(int position) {
-       /* // verify whether action mode is enabled or not
-        // if enabled, change the row state to activated
-        if (adapter.getSelectedItemCount() > 0) {
-            enableActionMode(position);
-        } else {
-            // read the message which removes bold from the row
-            Entity message = localEntity.get(position);
-            localEntity.set(position, message);
-            adapter.notifyDataSetChanged();
-        }*/
-    }
-
-   /* private class ActionModeCallback implements android.support.v7.view.ActionMode.Callback {
-        @Override
-        public boolean onCreateActionMode(android.support.v7.view.ActionMode mode, Menu menu) {
-            mode.getMenuInflater().inflate(R.menu.menu_contextual_mode, menu);
-
-            return true;
-        }
-
-        @Override
-        public boolean onPrepareActionMode(android.support.v7.view.ActionMode mode, Menu menu) {
-            return false;
-        }
-
-        @Override
-        public boolean onActionItemClicked(android.support.v7.view.ActionMode mode, MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.action_delete:
-                    // delete all the selected messages
-                    deleteMessages();
-                    mode.finish();
-                    return true;
-
-                default:
-                    return false;
-            }
-        }
-
-        @Override
-        public void onDestroyActionMode(android.support.v7.view.ActionMode mode) {
-            adapter.clearSelections();
-            actionMode = null;
-            recyclerView.post(new Runnable() {
-                @Override
-                public void run() {
-                    adapter.resetAnimationIndex();
-                    // mAdapter.notifyDataSetChanged();
-                }
-            });
-        }
-    }*/
-
-    // deleting the messages from recycler view
-    private void deleteMessages() {
-        adapter.resetAnimationIndex();
-        List<Integer> selectedItemPositions =
-                adapter.getSelectedItems();
-        for (int i = selectedItemPositions.size() - 1; i >= 0; i--) {
-            adapter.removeData(selectedItemPositions.get(i));
-        }
-        adapter.notifyDataSetChanged();
-
-        /*class ClientDeleteClass extends AsyncTask<String, Void, String> {
-
-            @Override
-            protected void onPreExecute() {
-                super.onPreExecute();
-
-                progressDialog2 = ProgressDialog.show(ClientDetailsActivity.this, "Loading Data",
-                        null, true, true);
-            }
-
-            @Override
-            protected void onPostExecute(String httpResponseMsg) {
-
-                super.onPostExecute(httpResponseMsg);
-
-                progressDialog2.dismiss();
-
-                Toast.makeText(ClientDetailsActivity.this, httpResponseMsg.toString(), Toast.LENGTH_LONG).show();
-
-            }
-
-            @Override
-            protected String doInBackground(String... params) {
-
-                // Sending Client id.
-                hashMap.put("id",IdHolder);
-
-                finalResult = httpParse.postRequest(hashMap, HttpUrlDeleteRecord);
-
-                return finalResult;
-            }
-        }
-
-        ClientDeleteClass ClientDeleteClass = new ClientDeleteClass();
-
-        ClientDeleteClass.execute(ClientID);*/
-    }
 }
