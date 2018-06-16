d663e32118d219c0c5fd644a78a68c0e841e1eef
aakanksha-gr <j.aakanksha007@gmail.com>
2018-06-16 13:21:35 +0530

    Database updated files

---
+++ app/src/main/java/com/pcdgroup/hp/pcd_group/DatabaseComponents/DataGetUrl.java
@@ -0,0 +1,37 @@
+package com.pcdgroup.hp.pcd_group.DatabaseComponents;
+
+/**
+ *
+ */
+
+public enum DataGetUrl {
+
+  GET_CLIENT_DETAILS,
+  DELETE_MULTIPLE_CLIENT,
+  CLIENT_LIST,
+  CLIENT_REGISTER,
+  DELETE_PRODUCTS,
+  EDIT_PRODUCTS;
+
+  private String[] urls = {
+    "http://dert.co.in/gFiles/ClientDetails.php",
+    "http://dert.co.in/gFiles/deletemultiple.php",
+    "http://dert.co.in/gFiles/DataClient.php",
+    "http://dert.co.in/gFiles/ClientRegister.php",
+    "",
+
+
+  };
+
+  public String getUrl(DataGetUrl getUrl){
+
+    //Get the relevant Url for the Data.
+    if(getUrl != null) {
+      return urls[getUrl.ordinal()];
+    }
+    else {
+      return null;
+    }
+
+  }
+}
